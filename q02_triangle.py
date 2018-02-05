25a096b9f8fc4f6c5c28f3e6782851d79a2c7796
Gi Soong Chee <gi.soongchee@dhs.sg>
2018-02-05 13:24:57 +0800

    added question in comments
    make program more generic :)

--- q02_triangle.py
+++ q02_triangle.py
@@ -6,7 +6,7 @@ side3 = int(input("Enter side 3: "))
 
 sum = side1 + side2
 
-if sum > side3:
+if sum > side3: # is this generic? what if side2 + side3 < side1?
     perimeter = side1 + side2 + side3
     print("Perimeter =", perimeter)
 else:
